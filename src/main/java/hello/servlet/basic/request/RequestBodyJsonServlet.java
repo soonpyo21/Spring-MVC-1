5cb92a79b8788762018c55b897b2f9e7408e54a8
soonpyo21 <tnsvy2004@gmail.com>
2022-05-29 20:16:54 +0900

    HttpServletResponse - 기본 사용법

--- src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -12,7 +12,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-
+/**
+ * http://localhost:8080/request-body-json
+ *
+ * JSON 형식 전송
+ * content-type: application/json
+ * message body: {"username": "hello", "age": 20} *
+ */
 @WebServlet(name="requestBodyJsonServlet", urlPatterns = "/request-body-json")
 public class RequestBodyJsonServlet extends HttpServlet {
 
