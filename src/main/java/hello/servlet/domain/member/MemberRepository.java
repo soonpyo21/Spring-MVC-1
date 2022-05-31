44b7cdb53f4ffd3efc61a1167510c30c8f52be80
soonpyo21 <tnsvy2004@gmail.com>
2022-05-31 23:59:22 +0900

    서블릿으로 회원 관리 웹 애플리케이션 만들기

--- src/main/java/hello/servlet/domain/member/MemberRepository.java
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+ */
 public class MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
